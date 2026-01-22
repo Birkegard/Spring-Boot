ee56a4efd2d2ebfc59bd854e94cc77de3ea9bd03
Birkegard <birkegard@hotmail.se>
2026-01-22 13:32:54 +0100

    Lagt till tom konstruktor och toString.

--- src/main/java/se/iths/christoffer/springboot/model/User.java
+++ src/main/java/se/iths/christoffer/springboot/model/User.java
@@ -7,6 +7,9 @@ public class User {
     private String name;
     private int age;
 
+    public User() {
+    }
+
     public User(String username, String password, String name, int age) {
         this.username = username;
         this.password = password;
@@ -44,5 +47,15 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
